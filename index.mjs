4015ed36cde5deb1642c181c52cbcff29b99e873
Anass-Dr <anasdrissi77@gmail.com>
2024-09-24 22:44:18 +0100

    added mongoose option and instructions after installation

--- index.mjs
+++ index.mjs
@@ -28,6 +28,13 @@ function getUserInput() {
             choices: ['Sequelize'],
             when: (answers) => answers.database === 'MySQL'
         },
+        {
+            type: 'list',
+            name: 'databaseOrm',
+            message: 'Which ORM would you like to use?',
+            choices: ['Mongoose'],
+            when: (answers) => answers.database === 'MongoDB'
+        },
     ]);
 }
 
@@ -37,9 +44,16 @@ console.log("Welcome to Express.js project skeleton creator!");
 console.log("*".repeat(50));
 console.log();
 const answers = await getUserInput();
-const db = answers.database === 'MySQL' ? `-${answers.database.toLowerCase()}` : '';
+const db = answers.database === 'No database' ? '' : `-${answers.database.toLowerCase()}`;
 
 if (answers.projectName === '.') shell.exec(`rm -Rf * && rm -Rf .* && rm * && rm .*`);
 shell.exec(`git clone -b version/express${db} https://github.com/Anass-Dr/Nodejs-Skeletons.git ${answers.projectName}`);
 shell.exec(`npm i`);
+// Add Instructions after installation
+console.log("*".repeat(50));
+console.log(`Your project has been created successfully!`);
+console.log(`To start your project, run the following commands:`);
+console.log(`> cd ${answers.projectName}`);
+console.log("Configure your environment variables in the .env file.");
+console.log(`> node server.js`);
 shell.exec(`rm -Rf .git`);
